fc58ae3d11aa6e63cb7dba9154268c204c4c95aa
Aditya Naga Sanjeevi, Yellapu <sanjeev.any@gmail.com>
2019-05-31 16:19:25 +0530

    Added Photon library for UI and created a toolbar layout with gridster

--- src/main/scripts/gridster.js
+++ src/main/scripts/gridster.js
@@ -1,17 +1,9 @@
 var gridster = [];
 
 $(function () {
-
-    gridster[0] = $("#left-bar ul").gridster({
-        namespace: '#left-bar',
-        widget_base_dimensions: [100, 55],
+    gridster[0] = $("#main-tool-menu ul").gridster({
+        namespace: '#main-tool-menu',
+        widget_base_dimensions: [20, 12],
         widget_margins: [0, 0]
     }).data('gridster');
-
-    gridster[1] = $("#demo-2 ul").gridster({
-        namespace: '#demo-2',
-        widget_base_dimensions: [200, 110],
-        widget_margins: [10, 10]
-    }).data('gridster');
-
 });
